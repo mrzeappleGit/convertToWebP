aad1c6f3b593bc4ab468916cc820be1c98ef87e3
Matthew Thomas Stevens <matthew.stevens@matthewstevens.me>
2023-08-16 23:55:32 -0500

    added updater and about tab

--- convertToWebP.py
+++ convertToWebP.py
@@ -8,7 +8,9 @@ import os
 import tkinter as tk
 from tkinter import  filedialog
 from tkinter import ttk
-from PIL import Image
+from tkinter import messagebox
+import webbrowser
+from PIL import Image, ImageTk
 import concurrent.futures
 import sv_ttk
 import time
@@ -16,6 +18,10 @@ import shutil
 from sys import platform
 from imageConverter import ImageConverterGUI
 from fileRenamer import FileRenamerGUI
+import requests
+import subprocess
+GITHUB_REPO_API = "https://api.github.com/repos/mrzeappleGit/convertToWebP/releases/latest"
+currentVersion = "1.4.0"
 
 class MainApp(tk.Tk):
     def __init__(self):
@@ -48,6 +54,15 @@ class MainApp(tk.Tk):
 
         self.file_renamer_button = ttk.Button(self.button_frame, text="File Renamer", command=self.show_file_renamer, cursor=cursor_point)
         self.file_renamer_button.pack(side="left", ipadx=10, ipady=10, padx=5, pady=5)
+        
+        # Hamburger menu button
+        self.menu_button = ttk.Button(self.button_frame, text="≡", command=self.show_menu)
+        self.menu_button.pack(side="right", padx=5, pady=5)
+        self.menu_button.config(width=1, cursor=cursor_point)
+        
+        # Dropdown menu for the hamburger menu button
+        self.dropdown_menu = tk.Menu(self, tearoff=0)
+        self.dropdown_menu.add_command(label="Check for Updates", command=self.check_and_update)
 
         self.image_converter = ImageConverterGUI(self)
         self.image_converter.pack(side="left", fill="both", expand=True)
@@ -58,6 +73,44 @@ class MainApp(tk.Tk):
         self.file_renamer.pack_forget()
 
         self.geometry('800x600')
+        is_update_available(currentVersion, 'github_owner_name', 'repository_name')
+        
+        # Check for updates on startup
+        self.update_available = self.check_for_updates_at_start()
+        
+        # Modify the hamburger menu button accordingly
+        self.update_menu_button_text()
+        
+        # Dropdown menu for the hamburger menu button
+        self.dropdown_menu = tk.Menu(self, tearoff=0)
+        self.update_dropdown_menu()
+        
+        
+    def check_for_updates_at_start(self):
+        # Check for updates
+        if is_update_available(currentVersion, 'mrzeappleGit', 'convertToWebP'):
+            return True
+        else:
+            return False
+        
+    def update_menu_button_text(self):
+        # Set button text based on whether an update is available
+        btn_text = "≡"
+        if self.update_available:
+            btn_text = "! " + btn_text
+        
+        # Update the text of the already initialized menu_button
+        self.menu_button.config(text=btn_text)
+
+    def update_dropdown_menu(self):
+        # Set menu text based on whether an update is available
+        menu_text = "Check for Updates"
+        if self.update_available:
+            menu_text = "! " + menu_text
+        
+        self.dropdown_menu.add_command(label=menu_text, command=self.check_and_update)
+        self.dropdown_menu.add_command(label="About", command=self.show_about)
+        
 
     def show_image_converter(self):
         cursor_point = "hand2" if platform != "darwin" else "pointinghand"
@@ -78,6 +131,10 @@ class MainApp(tk.Tk):
             self.attributes('-alpha', i/10)
             self.update()
             time.sleep(0.05)
+            
+    def show_menu(self):
+        # Display the dropdown menu below the menu button
+        self.dropdown_menu.post(self.menu_button.winfo_rootx(), self.menu_button.winfo_rooty() + self.menu_button.winfo_height())
 
     def show_file_renamer(self):
         cursor=cursor_point = "hand2" if platform != "darwin" else "pointinghand"
@@ -98,6 +155,127 @@ class MainApp(tk.Tk):
             self.attributes('-alpha', i/10)
             self.update()
             time.sleep(0.05)
+            
+    def check_and_update(self):
+        if is_update_available(currentVersion, "mrzeappleGit", "convertToWebP"):  # replace '1.0.0' with your current version
+            answer = messagebox.askyesno("Update Available", "An update is available. Do you want to download and install it?")
+            if answer:
+                download_update("mrzeappleGit", "convertToWebP")
+                apply_update()
+                messagebox.showinfo("Update Successful", "The application was updated successfully. Please restart the application to use the new version.")
+                self.quit()
+        else:
+            messagebox.showinfo("No Update", "You are using the latest version.")
+            
+    def show_about(self):
+        about_win = tk.Toplevel(self)
+        about_win.title("About")
+        def resource_path(relative_path):
+            try:
+            # PyInstaller creates a temp folder and stores path in _MEIPASS
+                base_path = sys._MEIPASS
+            except Exception:
+                base_path = os.path.abspath(".")
+                
+            return os.path.join(base_path, relative_path)
+
+        # Set the icon for the About window
+        iconPath = resource_path('convertToWebPIcon.ico')
+        about_win.iconbitmap(iconPath)
+
+        # Load and display the image
+        image_path = resource_path('convertToWebPLogo.png')
+        logo_image = Image.open(image_path)
+        # Resize the image
+        desired_size = (256, 256)  # Set width and height as needed
+        logo_image = logo_image.resize(desired_size, Image.Resampling.LANCZOS)
+        logo_photo = ImageTk.PhotoImage(logo_image)
+        logo_label = ttk.Label(about_win, image=logo_photo)
+        logo_label.image = logo_photo  # Keep a reference to avoid garbage collection
+        logo_label.pack(pady=10)
+
+        # Create and pack widgets for the version, copyright, and link to GitHub
+        ttk.Label(about_win, text="Version: " + currentVersion).pack(pady=5)
+        copyright = ttk.Label(about_win, text="©2023 Matthew Thomas Stevens Studios LLC", cursor="hand2", foreground="white", font="TkDefaultFont 10 underline")
+        copyright.pack(pady=5)
+        copyright.bind("<Button-1>", lambda e: webbrowser.open("https://www.matthewstevens.me"))
+        
+        link_label = ttk.Label(about_win, text="Visit GitHub Repo", cursor="hand2", foreground="white", font="TkDefaultFont 10 underline")
+        link_label.pack(pady=5)
+        link_label.bind("<Button-1>", lambda e: webbrowser.open("https://github.com/mrzeappleGit/convertToWebP"))
+        
+        about_win.geometry('300x400')  # Adjusted the size for the image
+        about_win.mainloop()
+        
+
+            
+def download_update(owner, repo):
+    try:
+        response = requests.get(GITHUB_REPO_API.format(owner=owner, repo=repo))
+        data = response.json()
+        
+        # Extract the URL of the .exe file from the assets of the latest release
+        exe_url = None
+        for asset in data.get("assets", []):
+            if asset.get("name", "").endswith(".exe"):
+                exe_url = asset.get("browser_download_url")
+                break
+        
+        if not exe_url:
+            print("No .exe file found in the latest release.")
+            return
+        
+        response = requests.get(exe_url, stream=True)
+        with open('latest_app.exe', 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+        return True
+    except Exception as e:
+        print(f"Error downloading update: {e}")
+        return False
+    
+def apply_update():
+    try:
+        # Rename the downloaded exe to a temporary name
+        os.rename('latest_app.exe', 'update_temp.exe')
+        
+        # Create the helper script
+        with open('update_helper.bat', 'w') as bat_file:
+            bat_content = """
+@echo off
+timeout /t 5 /nobreak
+move /y update_temp.exe convertToWebP.exe
+start convertToWebP.exe
+del update_helper.bat
+"""
+            bat_file.write(bat_content)
+        
+        # Start the helper script to handle the replacement without showing the command prompt
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        subprocess.Popen(['update_helper.bat'], startupinfo=startupinfo)
+        
+        # Close the current application
+        sys.exit(0)
+        
+    except Exception as e:
+        print(f"Error applying update: {e}")
+        return False
+
+            
+def is_update_available(current_version, owner, repo):
+        try:
+            response = requests.get(GITHUB_REPO_API.format(owner=owner, repo=repo))
+            data = response.json()
+            latest_version = data.get('tag_name', "").strip()
+            
+            # Assuming version format is like "v1.0.0", remove the "v" for comparison
+            if latest_version.startswith("v"):
+                latest_version = latest_version[1:]
+            
+            return latest_version > current_version
+        except:
+            return False
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
